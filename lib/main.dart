e51233d31e8f6b88b5846bcb280a1149c948a555
xyzrw <sajeewadw@gmail.com>
2023-04-02 23:39:30 +0530

    dynamic user login(logic) added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_firebase_test/pages/detail_page_event.dart';
 import 'package:flutter_firebase_test/pages/home_page.dart';
 
 import 'package:flutter_firebase_test/screens/authscreen.dart';
@@ -10,11 +11,48 @@ import 'package:flutter_firebase_test/screens/home.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  var userRole;
+
+  void getDataFromFirestore(String uid) {
+    // Initialize Firestore
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+    try {
+      var user = FirebaseFirestore.instance
+          .collection('users')
+          .doc(uid)
+          .get()
+          .then((value) => userRole = value.data()!['role'])
+          .catchError((error) {
+        print('Error reading data from Firestore: $error');
+      });
+      print(userRole);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<bool> checkUserRole(String uid) async {
+    try {
+      var user =
+          await FirebaseFirestore.instance.collection("users").doc(uid).get();
+
+      print(uid);
+
+      userRole = user['role'];
+
+      return true;
+
+      // print(userRole);
+    } catch (e) {
+      print(e.toString());
+      return false;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +66,28 @@ class MyApp extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (ctx, userSnapshot) {
           if (userSnapshot.hasData) {
-            return HomePage();
+            return FutureBuilder(
+                future: checkUserRole(userSnapshot.data!.uid),
+                builder: (contxt, snapshot) {
+                  if (userRole == "student") {
+                    return HomePage();
+                  } else if (userRole == "admin") {
+                    return DetailPageEvent();
+                  } else {
+                    return AuthScreen();
+                  }
+                });
+
+            // checkUserRole(userSnapshot.data!.uid);
+            // getDataFromFirestore(userSnapshot.data!.uid);
+
+            // if (userRole == "student") {
+            //   return HomePage();
+            // } else if (userRole == "admin") {
+            //   return DetailPageEvent();
+            // } else {
+            //   return AuthScreen();
+            // }
           } else {
             return AuthScreen();
           }
