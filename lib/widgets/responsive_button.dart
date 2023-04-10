64fb31635514b1b6c631d59ff0968dbf266aae1f
methjaya <methjaya252@gmail.com>
2023-04-10 19:31:19 +0530

    dot,Upper Text,lowerText added for Events

--- lib/widgets/responsive_button.dart
+++ lib/widgets/responsive_button.dart
@@ -1,36 +1,85 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_firebase_test/misc/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:flutter_firebase_test/misc/colors.dart';
-import 'package:flutter_firebase_test/widgets/app_large_text.dart';
-import 'package:flutter_firebase_test/widgets/app_text.dart';
-import 'package:flutter_firebase_test/widgets/responsive_button.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
 
 class ResponsiveButton extends StatelessWidget {
   final bool? isResponsive;
   final double? width;
-  ResponsiveButton({Key? key,this.width,this.isResponsive=false}) : super(key: key);
+  ResponsiveButton({Key? key, this.width, this.isResponsive = false})
+      : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Flexible(
-      child: Container(
-        width: width,
-        height:60,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Color.fromRGBO(73, 141, 56, 1),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          
-          children: [
-            AppLargeText(text: "Inquire",color:Colors.white,size:20,),
-          ],
+      child: GestureDetector(
+        onTap: () async {
+          const url = 'https://www.google.com';
+          if (await canLaunch(url)) {
+            print('Launching URL...');
+            await launch(url);
+            print('URL launched successfully!');
+          } else {
+            throw 'Could not launch $url';
+          }
+        },
+        child: Container(
+          width: width,
+          height: 60,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Color.fromRGBO(73, 141, 56, 1),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Inquire",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+
+//-------------
+
+// class ResponsiveButton extends StatelessWidget {
+//   final bool? isResponsive;
+//   final double? width;
+//   ResponsiveButton({Key? key,this.width,this.isResponsive=false}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context){
+//     return Flexible(
+//       child: GestureDetector(
+//         onTap: () {
+//           launch('https://www.google.com');
+//         },
+//         child: Container(
+//           width: width,
+//           height:60,
+//           decoration: BoxDecoration(
+//             borderRadius: BorderRadius.circular(10),
+//             color: Color.fromRGBO(73, 141, 56, 1),
+//           ),
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               AppLargeText(text: "Inquire",color:Colors.white,size:20,),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
